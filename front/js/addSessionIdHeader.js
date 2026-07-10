8ca73d85da0c19c64025039848ba8ba06ac7dc4f
Emmy VILLARD <emmy.villard.pro@proton.me>
2026-07-10 13:06:42 +0200

    feat: implement and test addSessionIdHeader - addSessionHeader.js - addSessionHeader.test.js - getUniqueSessionId.js (not functional)

---
+++ front/js/addSessionIdHeader.js
@@ -0,0 +1,24 @@
+import { getUniqueSessionId } from "./getUniqueSessionId.js"
+
+/**
+ * Adds (or overwrites) the 'X-Session-Id' header on the given request.
+ *
+ * No copy is made: this function does not copy the object passed as a
+ * parameter, it mutates it in place (via side effect). If the header
+ * already exists, it is overwritten with a new unique session id.
+ *
+ * @param {Request} request - The request object to mutate.
+ * @returns {Request} - The same request instance, with the session id header set.
+ */
+export function addSessionIdHeader(request) {
+  const headers = request.headers
+  const header_name = 'X-Session-Id'
+  if(headers.has(header_name)) {
+    console.debug(`Overwriting ${header_name} header`)
+    headers.set(header_name, getUniqueSessionId())
+  }
+  else {
+    headers.append(header_name, getUniqueSessionId())
+  }
+  return request
+}
---
+++ front/js/getUniqueSessionId.js
@@ -0,0 +1,3 @@
+export function getUniqueSessionId() {
+  return '123'
+}
